8d49d0bdcafd113f5498a0e74b6b10165ab8d167
yuan5201314-C <2021565746@qq.com>
2026-06-18 10:19:23 +0800

    Updates

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'wheat-v1'
+const urlsToCache = ['/', '/index.html']
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)))
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  )
+})
